38cc8ef505623d7593122ef1732f4c1bb223f688
kushalkumarcs372 <151655669+kushalkumarcs372@users.noreply.github.com>
2026-04-19 12:40:33 +0530

    Remove reportWebVitals and performance logging
    Removed reportWebVitals import and related comments.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import '@fontsource/playfair-display/600.css';
 import '@fontsource/playfair-display/700.css';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,7 +16,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
